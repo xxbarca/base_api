74040ae5b8f3c0c15fad14e5b26e471913ae7bbe
liyang <xxbarca@163.com>
2025-05-24 22:14:05 +0800

    update config

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
-import * as dotenv from 'dotenv';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: `.env.${process.env.NODE_ENV}`,
-      load: [() => dotenv.config({ path: '.env' })],
+      envFilePath: [`.env.${process.env.NODE_ENV}`, '.env'],
     }),
   ],
   controllers: [AppController],
